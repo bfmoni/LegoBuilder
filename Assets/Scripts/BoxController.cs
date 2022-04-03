b39c32bdd1c6a4452fbc1080198a98ce7ae6cacc
bfmoni <bfmoni68@gmail.com>
2022-04-03 17:53:20 -0500

    First commit

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public Material boxMaterial;
+    UnityEngine.Color gold = new Color32(233, 194, 26, 255);
+    UnityEngine.Color blue = new Color32(75, 197, 214, 255);
+    UnityEngine.Color red = new Color32(185, 14, 40, 255);
+    bool rotate; bool move;
+    float speed; float rotateSpeed;
+    bool selected; bool rotateSelected; bool colorSelected; bool moveSelected;
+
+
+    void Start () 
+    {
+        boxMaterial = GetComponent<Renderer>().material;
+        rotate = false;
+        move = false;
+        selected = false;
+        rotateSelected = false;
+        colorSelected = false;
+        moveSelected = false;
+        speed = 0.8f; rotateSpeed = 30.0f;
+    }
+
+    void Update () 
+    {
+        if(selected && Input.GetButton("Fire1") && rotateSelected)
+        {
+            rotate = true;
+        }
+        else
+        {
+            rotate = false;
+        }
+
+        if(selected && Input.GetButton("Fire1") && moveSelected)
+        {
+            move = true;
+        }
+        else
+        {
+            move = false;
+        }
+
+        if(selected && Input.GetButtonDown("Fire1") && colorSelected)
+        {
+            if(boxMaterial.color == gold)
+            {
+                boxMaterial.color = blue;
+            }
+            else if(boxMaterial.color == blue)
+            {
+                boxMaterial.color = red;
+            }
+            else
+            {
+                boxMaterial.color = gold;
+            }
+        }
+
+        if(rotate)
+        {
+            transform.Rotate(0, rotateSpeed*Time.deltaTime, 0);
+        }
+        if(move)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z + transform.position.z * speed * Time.deltaTime));
+        }
+    }
+
+
+    public void Unselected()
+    {
+        rotate = false;
+        move = false;
+        selected = false;
+    }
+
+    public void Select()
+    {
+        if(!selected)
+        {
+            selected = true;
+        }
+    }
+    public void SelectMove()
+    {
+        if(!moveSelected)
+        {
+            moveSelected = true;
+        }
+    }
+    public void SelectRotate()
+    {
+        if(!rotateSelected)
+        {
+            rotateSelected = true;
+        }
+    }
+    public void SelectColor()
+    {
+        if(!colorSelected)
+        {
+            colorSelected = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 5;
+    private Vector3 moveDirection;
+    private CharacterController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller=GetComponent<CharacterController>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float xDir = Input.GetAxisRaw("Horizontal");
+        float yDir = Input.GetAxisRaw("Vertical");
+        if(Input.GetButton("Jump")) //back button is currently joystick button 3
+        {
+           
+            moveDirection=new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+            moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            moveDirection.z = 0.0f;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+        else
+        {
+            moveDirection=new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+            moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            moveDirection.y = 0.0f;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+
+        
+        
+
+    }
+    void OnTriggerEnter(Collider e)
+    {
+        if(e.transform.position.x > transform.position.x)
+        {
+            transform.position = new Vector3(transform.position.x - 0.3f,1.5f,transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x + 0.3f,1.5f,transform.position.z);
+        }
+        if(e.transform.position.z > transform.position.z)
+        {
+            transform.position = new Vector3(transform.position.x,1.5f,transform.position.z- 0.3f);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x,1.5f,transform.position.z + 0.3f);
+        }
+        
+    }
+}
